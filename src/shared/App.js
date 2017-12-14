e26d4880deaebc0d95cd4ca9beb1b6a6f4742a2c
ZNZ <nxz84@case.edu>
2017-12-14 09:40:58 -0500

    first commit

---
+++ src/shared/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="APP">
+        <div className="kk">
+          <h2>Welcome to React in the server</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
